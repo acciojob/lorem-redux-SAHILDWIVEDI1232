f184060ccabb2cd7b38d69121d79b3ed35b2652a
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-04-30 10:47:04 +0530

    Update import path for fetchData action

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
-
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchData } from "../redux/actions";
+import { fetchData } from "../actions";
 import './../styles/App.css';
 
 const App = () => {
