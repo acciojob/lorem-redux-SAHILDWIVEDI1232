ec33e3309b75b5eaf04df4e31c0332e6a3ec72dc
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-04-30 10:38:32 +0530

    Integrate Redux for data fetching in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../redux/actions";
 import './../styles/App.css';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { loading, data, error } = useSelector(state => state);
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      {loading && <p>Loading...</p>}
+
+      {error && <p>Error: {error}</p>}
+
+      {data && (
+        <p>
+          <strong>{data.title}</strong><br />
+          {data.body}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default App;
